26393160f679b7ddaab9d6386ac976cf33d6a12d
abdullah abbas <abdullahalhoothy92@gmail.com>
2025-11-01 12:24:42 -0400

    feat: Add shipping details to email content and remove unused scripts

--- run.py
+++ run.py
@@ -10,16 +10,6 @@ from send_mass_email_smtp import send_bulk_emails
 
 
 def main():
-    # Create an instance with default file paths
-    # sender = GmailMassSender(
-    #     emails_file="emails.txt",
-    #     content_file="email_content.txt",
-    #     attachments_dir="attachments",
-    # )
-
-    # Run the mass email sender
-    # sender.run()
-
     send_bulk_emails(
         contacts_file="canada_licensed_customs_brokers.csv",
     )
